48e891156716ab8d1179b4a5d75f2fa085adfdf6
Ak5cel <akhilaficel@gmail.com>
2023-11-21 17:10:05 +0530

    implement GET /api/articles

--- app.js
+++ app.js
@@ -2,13 +2,15 @@ const express = require("express");
 const { getTopics } = require("./controllers/topics.controllers");
 const { handleFourOhFour, getApi } = require("./controllers/api.controllers");
 const { handleServerErrors, handleCustomErrors, handlePostgresErrors } = require("./errors");
-const { getArticleById } = require("./controllers/articles.controllers");
+const { getArticleById, getArticles } = require("./controllers/articles.controllers");
 
 const app = express();
 
 app.get("/api", getApi);
 
 app.get("/api/topics", getTopics);
+
+app.get("/api/articles", getArticles);
 
 app.get("/api/articles/:article_id", getArticleById);
 
--- models/articles.models.js
+++ models/articles.models.js
@@ -1,5 +1,27 @@
 const db = require("../db/connection.js");
 
+exports.selectArticles = () => {
+  const queryString = `
+  SELECT 
+    articles.article_id, 
+    articles.author, 
+    articles.title, 
+    articles.topic, 
+    articles.created_at, 
+    articles.votes, 
+    articles.article_img_url, 
+    COUNT(comments.comment_id)::INT as comment_count
+  FROM articles LEFT JOIN comments
+    ON articles.article_id = comments.article_id
+  GROUP BY articles.article_id
+  ORDER BY articles.created_at DESC;
+  `;
+
+  return db.query(queryString).then(({ rows }) => {
+    return rows;
+  });
+};
+
 exports.selectArticleById = (article_id) => {
   return db.query("SELECT * FROM articles WHERE article_id=$1;", [article_id]).then(({ rows }) => {
     if (!rows.length) {
